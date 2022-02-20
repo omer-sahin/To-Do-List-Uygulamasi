f6e94ed485ed57ce68036168350a4f99555b5ecf
Ömer ŞAHİN <omers4hin04@gmail.com>
2022-02-21 02:36:14 +0300

    Proje dosyası ekle.

---
+++ To Do List Uygulaması/app.js
@@ -0,0 +1,36 @@
+﻿const AddButton = document.querySelector('.Add-Button');
+const ClearButton = document.querySelector('.clear-Button');
+const Container = document.querySelector('.container');
+const inputText = document.querySelector('.input-text');
+const TodoContainer = document.getElementById('Todo-Container');
+AddButton.addEventListener('click', function (e) {
+    let pharagraph = document.createElement('p');
+    pharagraph.classList.add('pharagraph')
+    TodoContainer.appendChild(pharagraph);
+    pharagraph.innerHTML = inputText.value;
+    inputText.value = '';
+    pharagraph.addEventListener('click', function () {
+        pharagraph.style.textDecoration = 'line-through';
+    })
+    pharagraph.addEventListener('dblclick', function () {
+
+        pharagraph.remove();
+    });
+
+    ClearButton.addEventListener('click', function () {
+
+        //pharagraph.style.display = 'none';
+
+        //pharagraph.remove();
+        TodoContainer.removeChild(pharagraph);
+    });
+
+});
+
+
+
+
+
+
+
+
